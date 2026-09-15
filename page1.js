50d4777f6ffa2daeb5b6aa7efb49f94f86fcc5cf
Frederick Corpuz <fcorpuz@wesleyan.edu>
2026-09-15 16:23:16 +0800

    Move project #1 tiffy's log to a separate page

--- page1.js
+++ page1.js
@@ -1,4 +1,3 @@
-// 1. Grab the save-el paragrah and store it in a variable called saveEl
 let countEl = document.getElementById("count-el")
 let saveEl = document.getElementById("save-el")
 let count = 0
